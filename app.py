c0fba25a07bc956d9e8222f3c1b763a9074dc21a
Peter Fogg <pfogg@Mac.lan>
2025-12-30 15:18:18 -0700

    Allow uploading a CSV to create classes.

--- app.py
+++ app.py
@@ -1,44 +1,52 @@
+import csv
+from io import BytesIO, StringIO
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Integer, DateTime
+from sqlalchemy import String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.utils import secure_filename
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
-print(app.config['SQLALCHEMY_DATABASE_URI'])
+
+app.config['TEMPORARY_UPLOAD_DIR'] = os.path.join(basedir, 'tempfiles')
+
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-class Student(db.Model):
-    id = mapped_column(Integer, primary_key=True)
-    name = mapped_column(String)
-
 
 class Course(db.Model):
     id = mapped_column(Integer, primary_key=True)
     name = mapped_column(String, unique=True)
-    students = db.relationship('Student', backref='course')
-    sections = db.relationship('Section', backref='course')
+    students = db.relationship('Student', back_populates='course')
+    # sections = db.relationship('Section', backref='course')
 
 
-class Section(db.Model):
+class Student(db.Model):
     id = mapped_column(Integer, primary_key=True)
-    datetime = mapped_column(DateTime)
+    name = mapped_column(String)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    course = db.relationship('Course', back_populates='students')
 
 
-class Interaction(db.Model):
-    id = mapped_column(Interaction, primary_key=True)
-    student = mapped_column(ForeignKey('student.id'))
-    section = mapped_column(ForeignKey('section.id'))
-    count = mapped_column(Integer)
+# class Section(db.Model):
+#     id = mapped_column(Integer, primary_key=True)
+#     datetime = mapped_column(DateTime)
+
+
+# class Interaction(db.Model):
+#     id = mapped_column(Integer, primary_key=True)
+#     student = mapped_column(ForeignKey('student.id'))
+#     section = mapped_column(ForeignKey('section.id'))
+#     count = mapped_column(Integer)
 
 
 with app.app_context():
@@ -47,6 +55,25 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return 'hi there'
+    courses = db.session.execute(db.select(Course)).scalars()
+    return render_template('index.html', courses=courses)
 
+
+@app.route('/create_class', methods=["POST"])
+def create_class():
+    if 'class' in request.files:
+        class_name = request.form['class-name']
+        course = Course(name=class_name)
+        f = request.files['class']
+        path = os.path.join(app.config['TEMPORARY_UPLOAD_DIR'], secure_filename(f.name))
+        f.save(path)
+        with open(path, 'r', encoding='utf-8') as fp:
+            reader = csv.DictReader(fp)
+            for row in reader:
+                full_name = ' '.join([row['first_name'], row['last_name']])
+                student = Student(name=full_name, course=course)
+                db.session.add(student)
+        db.session.commit()
+        os.remove(path)
+    return redirect(url_for('index'))
 
